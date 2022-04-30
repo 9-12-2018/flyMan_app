f1cde612690f68c7f93c8a4c26bbd161d6391797
dtlucho <luciano.ditomaso@gmail.com>
2022-04-29 22:16:20 -0300

    Log in screen layout

---
+++ screens/LogInScreen/index.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+
+export default function LogInScreen() {
+    return (
+        <SafeAreaView>
+            <Text>LogInScreen</Text>
+        </SafeAreaView>
+    )
+}
