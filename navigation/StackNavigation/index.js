0b82588893c191313569eaef1f6c2edaaa6bf573
dtlucho <luciano.ditomaso@gmail.com>
2022-05-04 22:18:30 -0300

    auth flow

--- navigation/StackNavigation/index.js
+++ navigation/StackNavigation/index.js
@@ -1,27 +1,39 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/HomeScreen';
 import CarDetailScreen from '../../screens/CarDetailsScreen';
 import LogInScreen from '../../screens/LogInScreen';
+import GlobalContext from '../../components/globals/context'
 
 const Stack = createNativeStackNavigator();
 
 export default function index() {
+    const [token, setToken] = useState('')
+
+    const isAuthenticated = () => token;
+
+    const authUser = (token) => {
+        if (!token) throw new Error();
+        console.log('Aca deberia ir al secure storage');
+        setToken(token);
+    }
+
     return (
-        <Stack.Navigator>
-            {
-                (true)
-                    ? (
-                        <>
-                            <Stack.Screen name='Home' component={HomeScreen} />
-                            <Stack.Screen name='Car Detail' component={CarDetailScreen} options={{ headerBackTitle: 'Volver' }} />
-                        </>
-                    )
-                    : (
-                        <Stack.Screen name='Log In' component={LogInScreen} />
-                    )
-            }
-        </Stack.Navigator>
+        <GlobalContext.Provider value={{ authUser }}>
+            <Stack.Navigator>
+                {
+                    (isAuthenticated())
+                        ? (
+                            <>
+                                <Stack.Screen name='Home' component={HomeScreen} />
+                                <Stack.Screen name='Car Detail' component={CarDetailScreen} options={{ headerBackTitle: 'Volver' }} />
+                            </>
+                        )
+                        : (
+                            <Stack.Screen name='Log In' component={LogInScreen} />
+                        )
+                }
+            </Stack.Navigator>
+        </GlobalContext.Provider>
     )
 }
