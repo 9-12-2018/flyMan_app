193f21c8c41d8f1be91eae021a2e94595cfe1db0
dtlucho <luciano.ditomaso@gmail.com>
2022-05-08 18:12:01 -0300

    saving token in storage

--- navigation/StackNavigation/index.js
+++ navigation/StackNavigation/index.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../../screens/HomeScreen';
 import CarDetailScreen from '../../screens/CarDetailsScreen';
 import LogInScreen from '../../screens/LogInScreen';
-import GlobalContext from '../../components/globals/context'
+import GlobalContext from '../../components/globals/context';
+import { saveToken, retrieveToken, removeToken } from '../../services/secureStorage';
 
 const Stack = createNativeStackNavigator();
 
 export default function index() {
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState('');
+
+    useEffect(async () => {
+        const token = await checkToken();
+        setToken(token);
+    }, []);
+
 
     const isAuthenticated = () => token;
 
@@ -16,6 +24,17 @@ export default function index() {
         if (!token) throw new Error();
         console.log('Aca deberia ir al secure storage');
         setToken(token);
+        saveToken(token);
+    }
+
+    const checkToken = async () => {
+        const token = await retrieveToken();
+        return token;
+    }
+
+    const logout = async () => {
+        await removeToken();
+        setToken(null);
     }
 
     return (
@@ -25,7 +44,13 @@ export default function index() {
                     (isAuthenticated())
                         ? (
                             <>
-                                <Stack.Screen name='Home' component={HomeScreen} />
+                                <Stack.Screen
+                                    name='Home'
+                                    component={HomeScreen}
+                                    options={{
+                                        headerLeft: () => <Button title={"Log out"} onPress={async () => { await logout() }} />
+                                    }}
+                                />
                                 <Stack.Screen name='Car Detail' component={CarDetailScreen} options={{ headerBackTitle: 'Volver' }} />
                             </>
                         )
