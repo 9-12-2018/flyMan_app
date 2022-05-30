964aade181903bf3f4d94a7d92ff664922118e7e
ferminmorello <fermin@mubee.io>
2022-05-30 20:53:03 -0300

    bReservations::Login

--- api/users.js
+++ api/users.js
@@ -3,6 +3,7 @@ import { BASE_URL } from '@env'
 
 const login = async (email, password) => {
     const url = `${BASE_URL}/users/login`
+    console.log(BASE_URL);
     let response;
     try {
         response = await axios.post(url, { email, password });
